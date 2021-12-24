b163dd7df79991968a08a50ca1aa24a56a2134e6
Alexey Savin <therealsava@icloud.com>
2021-12-24 22:26:45 +0300

    Scripted auto-commit on change (2021-12-24 22:26:45) by gitwatch.sh

--- src/main/java/ru/bmstu/ZWatcher.java
+++ src/main/java/ru/bmstu/ZWatcher.java
@@ -1,2 +1,9 @@
-package ru.bmstu;public class ZWatcher {
+package ru.bmstu;
+
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+
+public class ZWatcher implements Watcher {
+    private final ActorRef cfg;
+    private ZooKeeper zooKeeper;
 }
