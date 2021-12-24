dd1951e9a10147f02b7a626c7275b89a78369759
Alexey Savin <therealsava@icloud.com>
2021-12-24 22:36:14 +0300

    Scripted auto-commit on change (2021-12-24 22:36:13) by gitwatch.sh

--- src/main/java/ru/bmstu/ZAnonimApp.java
+++ src/main/java/ru/bmstu/ZAnonimApp.java
@@ -1,2 +1,14 @@
-package ru.bmstu;public class ZAnonimApp {
+package ru.bmstu;
+
+import org.apache.zookeeper.KeeperException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class ZAnonimApp {
+    private static List<ServerNode> serverNodes;
+
+    public static void main(String[] args) throws IOException, InterruptedException, KeeperException {
+        serverNodes = new ArrayList<>();
+    }
 }
