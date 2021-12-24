4c12c4364054b492dbf4b76073db5f108b4e40eb
Alexey Savin <therealsava@icloud.com>
2021-12-24 22:33:42 +0300

    Scripted auto-commit on change (2021-12-24 22:33:42) by gitwatch.sh

--- src/main/java/ru/bmstu/ZWatcher.java
+++ src/main/java/ru/bmstu/ZWatcher.java
@@ -1,9 +1,53 @@
 package ru.bmstu;
 
+import akka.actor.ActorRef;
+import akka.actor.dsl.Creators;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooKeeper;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ZWatcher implements Watcher {
     private final ActorRef cfg;
     private ZooKeeper zooKeeper;
+    private static final String SERVER_PATH = "/servers";
+
+    public  ZWatcher (ActorRef cfg, ZooKeeper zooKeeper) {
+        this.cfg = cfg;
+        this.zooKeeper = zooKeeper;
+    }
+
+    public ZWatcher (ActorRef cfg) {
+        this.cfg = cfg;
+    }
+
+    public void setZooKeeper(ZooKeeper zooKeeper) {
+        this.zooKeeper = zooKeeper;
+    }
+
+
+    @Override
+    public void process (WatchedEvent event) {
+        if (event == null) {
+            return;
+        }
+        Event.KeeperState keeperState = event.getState();
+        Event.EventType eventType = event.getType();
+        String path = event.getPath();
+        if (Event.KeeperState.SyncConnected == keeperState) {
+            try {
+                List<String> list = zooKeeper.getChildren(SERVER_PATH, this);
+                ArrayList<String> serverData = new ArrayList<>();
+                for (String name : list) {
+                    serverData.add(new String(zooKeeper.getData(SERVER_PATH + '/' + name, this, null)));
+                }
+                cfg.tell(new ServerList(serverData), ActorRef.noSender());
+            } catch (KeeperException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
