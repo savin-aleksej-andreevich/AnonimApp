127c1dcae015a5738dc3eb4baa1a078d8802017c
Alexey Savin <therealsava@icloud.com>
2021-12-24 22:55:14 +0300

    Scripted auto-commit on change (2021-12-24 22:55:14) by gitwatch.sh

--- src/main/java/ru/bmstu/ServerNode.java
+++ src/main/java/ru/bmstu/ServerNode.java
@@ -1,4 +1,58 @@
 package ru.bmstu;
 
-public class ServerNode {
+import akka.NotUsed;
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.http.javadsl.Http;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.http.javadsl.server.AllDirectives;
+import akka.http.javadsl.server.Route;
+import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.ZooDefs;
+import org.apache.zookeeper.ZooKeeper;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.CompletionStage;
+
+public class ServerNode extends AllDirectives {
+    private static ActorRef cfg;
+    private static Integer port;
+    private static ActorSystem system;
+    private static ZWatcher watcher;
+
+    public static void main(String[] args) throws IOException, InterruptedException, KeeperException {
+        port = Integer.parseInt(args[0]);
+        system = ActorSystem.create("routes");
+        //url = args[0]; system = ActorSystem.create("routes");
+        cfg = system.actorOf(Props.create(StorageActor.class));
+        watcher = new ZWatcher(cfg);
+        ZooKeeper zooKeeper = new ZooKeeper("127.0.0.1:2181", 3000, watcher);
+        watcher.setZooKeeper(zooKeeper);
+        zooKeeper.create("/servers/s",
+                port.toString().getBytes(),
+                ZooDefs.Ids.OPEN_ACL_UNSAFE,
+                CreateMode.EPHEMERAL_SEQUENTIAL);
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
+        ServerNode instance = new ServerNode();
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow;
+        routeFlow = instance.createRoute(system).flow(system, materializer);
+        final CompletionStage<S>
+    }
+
+    private Route createRoute (ActorSystem system) {
+        return route(
+                get()
+        );
+    }
+
+    private Route get() {
+
+    }
 }
