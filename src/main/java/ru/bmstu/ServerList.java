3de24c3b25630025eb1dc0ee970807346e42d94f
Alexey Savin <therealsava@icloud.com>
2021-12-24 22:24:09 +0300

    Scripted auto-commit on change (2021-12-24 22:24:09) by gitwatch.sh

--- src/main/java/ru/bmstu/ServerList.java
+++ src/main/java/ru/bmstu/ServerList.java
@@ -1,4 +1,13 @@
 package ru.bmstu;
 
 public class ServerList {
+    private List<String> data;
+
+    public ServerList(List<String> data) {
+        this.data = data;
+    }
+    public List<String> getData() {
+        return data;
+    }
+
 }
